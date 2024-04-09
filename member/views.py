7b56ca46bb3566db455821db18df5776f6c6658e
chorhksdlr <chorhksdlr@gmail.com>
2024-04-09 16:34:33 +0900

    qwe

--- member/views.py
+++ member/views.py
@@ -1065,6 +1065,7 @@ class MypagePayDeleteAPIVIEW(APIView):
                                    cancel_price=pay.price,
                                    cancel_username='관리자', cancel_message='취소됨')
 
+
         PayCancel.objects.create(pay_cancel_reason=reason)
         return Response('good')
 
